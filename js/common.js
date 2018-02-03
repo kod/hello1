3d3e8873e117061176a323bba0a691ad0602c19d
taichiyi <taichiyi@foxmail.com>
2018-02-03 13:42:00 +0800

    test(*): v2.5.1-alpha, 部署测试服

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ if (!window.F) window.F = {};
 
 F._DEUBUG = true;
 
-F._VERSION = "2.5.0";
+F._VERSION = "2.5.1-alpha";
 console.log("version " + F._VERSION);
 
 F._IP_255 = F._DEUBUG ? "http://192.168.1.100" : "https://vn255.buyoo.xyz";
