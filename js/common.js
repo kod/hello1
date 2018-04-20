71cbe3afdcea16c9388eaea87f56f9df47a91917
taichiyi <taichiyi@foxmail.com>
2018-04-20 14:00:20 +0800

    test(*): 2.8.0-alpha;部署测试服

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ if (!window.F) window.F = {};
 
 F._DEUBUG = true;
 
-F._VERSION = '2.7.1-alpha';
+F._VERSION = '2.8.0-alpha';
 console.log('version ' + F._VERSION);
 
 F._IP_255 = F._DEUBUG ? 'http://119.28.177.175' : 'https://vn255.buyoo.xyz';
